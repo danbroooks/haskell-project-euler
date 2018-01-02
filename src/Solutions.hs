fe787d94dbe5656ca40c284774960e52ad7b8875
Dan Brooks <dmbrooks@live.co.uk>
2018-01-02 12:59:56 +0000

    Add solution for second exercise

--- src/Solutions.hs
+++ src/Solutions.hs
@@ -1,5 +1,11 @@
 module Solutions where
 
-multiplesOfThreeAndFive :: Integer -> Integer
+multiplesOfThreeAndFive :: Int -> Int
 multiplesOfThreeAndFive n =
   sum [ x | x <- [1..n-1], x `mod` 5 == 0 || x `mod` 3 == 0 ]
+
+evenFibonacciNumbers :: Int -> Int
+evenFibonacciNumbers n = sum [ x | x <- takeWhile (< n) fibs, x `mod` 2 == 0 ]
+
+fibs :: [Int]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
--- test/Tests.hs
+++ test/Tests.hs
@@ -1,14 +1,21 @@
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 
-import Solutions (multiplesOfThreeAndFive)
+import Solutions
+  ( multiplesOfThreeAndFive
+  , evenFibonacciNumbers
+  )
 
 tests :: TestTree
 tests = testGroup "tests" testCases
   where
     testCases =
       [ testCase "multiplesOfThreeAndFive"
-      $ assertEqual "sum of numbers divisable by 3 and 5 less than 10" 23 (multiplesOfThreeAndFive 10)
+      $ uncurry (assertEqual "sum of numbers divisable by 3 and 5 less than 10")
+      (23, multiplesOfThreeAndFive 10)
+      , testCase "evenFibonacciNumbers"
+      $ uncurry (assertEqual "return the sum of all prime fib numbers up to four million")
+      (4613732, evenFibonacciNumbers 4000000)
       ]
 
 main :: IO ()
