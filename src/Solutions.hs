6789e0189df8e4f143ff7f9d89b4eef4dfc32335
Dan Brooks <dmbrooks@live.co.uk>
2018-04-11 11:29:56 +0100

    WIP

--- src/Solutions.hs
+++ src/Solutions.hs
@@ -1,11 +1,30 @@
 module Solutions where
 
+import qualified Data.List as L
+import           Safe
+
 multiplesOfThreeAndFive :: Int -> Int
 multiplesOfThreeAndFive n =
   sum [ x | x <- [1..n-1], x `mod` 5 == 0 || x `mod` 3 == 0 ]
 
 evenFibonacciNumbers :: Int -> Int
-evenFibonacciNumbers n = sum [ x | x <- takeWhile (< n) fibs, x `mod` 2 == 0 ]
+evenFibonacciNumbers n =
+  sum [ x | x <- takeWhile (< n) fibs, x `mod` 2 == 0 ]
+
+largestPrimeFactor :: Int -> Maybe Int
+largestPrimeFactor =
+  maximumMay . primeFactors
+
+primeFactors :: Int -> [Int]
+primeFactors n = L.unfoldr findP n
+  where
+    findP n =
+      
+
+primes :: [Int]
+primes =
+  2 : 3
 
 fibs :: [Int]
-fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+fibs =
+  0 : 1 : zipWith (+) fibs (tail fibs)
--- test/Tests.hs
+++ test/Tests.hs
@@ -4,6 +4,7 @@ import Test.Tasty.HUnit (assertEqual, testCase)
 import Solutions
   ( multiplesOfThreeAndFive
   , evenFibonacciNumbers
+  , largestPrimeFactor
   )
 
 tests :: TestTree
@@ -12,10 +13,22 @@ tests = testGroup "tests" testCases
     testCases =
       [ testCase "multiplesOfThreeAndFive"
       $ uncurry (assertEqual "sum of numbers divisable by 3 and 5 less than 10")
-      (23, multiplesOfThreeAndFive 10)
+      ( 23, multiplesOfThreeAndFive 10 )
+
       , testCase "evenFibonacciNumbers"
       $ uncurry (assertEqual "return the sum of all prime fib numbers up to four million")
-      (4613732, evenFibonacciNumbers 4000000)
+      ( 4613732, evenFibonacciNumbers 4000000 )
+
+      , testGroup "largestPrimeFactor"
+
+        [ testCase "the number 13195"
+        $ uncurry (assertEqual "the largest prime factor for 13195")
+        ( Just 29, largestPrimeFactor 13195 )
+
+        , testCase "the number 600851475143"
+        $ uncurry (assertEqual "the largest prime factor for 600851475143")
+        ( Just 6857, largestPrimeFactor 600851475143 )
+        ]
       ]
 
 main :: IO ()
